c892e33f9998959d6259cd3814bee41cc2067d14
Francisco Igor <franciscoigor@gmail.com>
2022-02-09 11:28:32 -0800

    caperucita part 2

--- scripts/newaudios.sh
+++ scripts/newaudios.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ "$1" == "" ]; then
   echo "Usage: $0 lang 'expr1 expr2''"
   echo "Example: $0 en 'hello world'"
@@ -11,9 +12,12 @@ do
   FILE=${FILE/ó/o}
   FILE=${FILE/ú/u}
   FILE=${FILE/\?/}
+  FILE=${FILE/¿/}
+  FILE=${FILE/!/}
+  FILE=${FILE/:/}
   FILE=${FILE/,/}
   FILE=${FILE/./}
-  FILE=$(echo $FILE | awk '{print tolower($0)}')
+  FILE=$(echo "$FILE" | awk '{print tolower($0)}')
   if [ ! -e www/audio/$1/$FILE.wav ]; then
     echo "Creating $WORD..."
     ./scripts/newaudio.sh $1 $WORD
